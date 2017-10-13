8c778cc6831167cbd7c3675ccedccd9479dfda13
Livalon <958969616@qq.com>
2017-10-13 14:15:15 +0800

    Led显示的实现

---
+++ test/test/Led.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace test
+{
+    partial class Led
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gaugeControl1 = new DevExpress.XtraGauges.Win.GaugeControl();
+            this.digitalGauge1 = new DevExpress.XtraGauges.Win.Gauges.Digital.DigitalGauge();
+            this.digitalBackgroundLayerComponent1 = new DevExpress.XtraGauges.Win.Gauges.Digital.DigitalBackgroundLayerComponent();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.digitalGauge1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.digitalBackgroundLayerComponent1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gaugeControl1
+            // 
+            this.gaugeControl1.AutoLayout = false;
+            this.gaugeControl1.Gauges.AddRange(new DevExpress.XtraGauges.Base.IGauge[] {
+            this.digitalGauge1});
+            this.gaugeControl1.Location = new System.Drawing.Point(0, 0);
+            this.gaugeControl1.Name = "gaugeControl1";
+            this.gaugeControl1.Size = new System.Drawing.Size(557, 409);
+            this.gaugeControl1.TabIndex = 0;
+            // 
+            // digitalGauge1
+            // 
+            this.digitalGauge1.AppearanceOff.ContentBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:#0FD4F2FF");
+            this.digitalGauge1.AppearanceOn.ContentBrush = new DevExpress.XtraGauges.Core.Drawing.SolidBrushObject("Color:#D4F2FF");
+            this.digitalGauge1.BackgroundLayers.AddRange(new DevExpress.XtraGauges.Win.Gauges.Digital.DigitalBackgroundLayerComponent[] {
+            this.digitalBackgroundLayerComponent1});
+            this.digitalGauge1.Bounds = new System.Drawing.Rectangle(6, 6, 392, 308);
+            this.digitalGauge1.DigitCount = 5;
+            this.digitalGauge1.Name = "digitalGauge1";
+            // 
+            // digitalBackgroundLayerComponent1
+            // 
+            this.digitalBackgroundLayerComponent1.BottomRight = new DevExpress.XtraGauges.Core.Base.PointF2D(259.8125F, 99.9625F);
+            this.digitalBackgroundLayerComponent1.Name = "b1";
+            this.digitalBackgroundLayerComponent1.ShapeType = DevExpress.XtraGauges.Core.Model.DigitalBackgroundShapeSetType.Style2;
+            this.digitalBackgroundLayerComponent1.TopLeft = new DevExpress.XtraGauges.Core.Base.PointF2D(20F, 0F);
+            this.digitalBackgroundLayerComponent1.ZOrder = 1000;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 421);
+            this.Controls.Add(this.gaugeControl1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.digitalGauge1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.digitalBackgroundLayerComponent1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGauges.Win.GaugeControl gaugeControl1;
+        private DevExpress.XtraGauges.Win.Gauges.Digital.DigitalGauge digitalGauge1;
+        private DevExpress.XtraGauges.Win.Gauges.Digital.DigitalBackgroundLayerComponent digitalBackgroundLayerComponent1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ test/test/Led.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.Data.Browsing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace test
+{
+    public partial class Led : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        List<String> values = new List<string>();
+        int index = 0;
+        public Led()
+        {
+            InitializeComponent();
+            var query = from t in db.LED1
+                            //where t.id == i
+                        select t;
+            foreach (LED1 item in query)
+            {
+                values.Add(item.data);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (index < values.Count)
+            { 
+                this.digitalGauge1.Text = values[index];
+                index++;
+            }
+            //DataClasses1DataContext db = new DataClasses1DataContext();
+            //int i = 1;
+            //while (i<3)
+            //{
+
+            //    var query = from t in db.LED1
+            //                //where t.id == i
+            //                select t;
+            //    foreach (LED1 item in query)
+            //    {
+            //        this.digitalGauge1.Text = item.data;
+            //    }
+            //    i++;
+            //}
+
+        }
+
+    }
+}
