fb4ddee673327ee708a9ec6d3953a90b9fe8e005
wangyanbo <1029246137@qq.com>
2017-10-25 16:29:18 +0800

    实时保存CSV数据文件

---
+++ CSV/test/test/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace test
+{
+    class Program
+    {
+        public void Read(string path)
+        {
+            FileStream fs = new FileStream("E:\\tes.csv", FileMode.Create);
+            StreamReader sr = new StreamReader(path, Encoding.Default);
+            StreamWriter sw = new StreamWriter(fs);
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Write(line,sw);
+            }
+            sw.Close();
+            fs.Close();
+        }
+        internal class byData
+        {
+        }
+        public void Write(string line, StreamWriter sw)
+        {
+            //获得字节数组
+            sw.WriteLine(line);
+
+
+        }
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            p.Read("E:\\tes.txt");
+
+        }
+    }
+}
+
